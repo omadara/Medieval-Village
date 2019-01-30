143efddfc6503a1d77c6f40cc3d60655b8150ec2
MadProgrammerGR <giorgozombo@yahoo.gr>
2019-01-30 20:03:51 +0200

    better terrain ground, buildings details and animatable doors

---
+++ Assets/Prefabs/Door/DoorController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    // toggle open/close on click
+    void OnMouseDown() {
+        animator.SetBool("open", !animator.GetBool("open"));
+    }
+}
