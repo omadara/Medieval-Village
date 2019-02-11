63ac4ceee69d24727f730296c36663d945537b77
fanis1996 <fanis1996GR20@hotmail.com>
2019-02-11 15:05:30 +0200

    added navmesh, walk animation for knight

---
+++ Assets/Prefabs/Characters/Knight/Animation/Patrol.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Animator))]
+
+public class Patrol : MonoBehaviour
+{
+    Animator anim;
+    Vector2 smoothDeltaPosition = Vector2.zero;
+    Vector2 velocity = Vector2.zero;
+    private GameObject[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        points = GameObject.FindGameObjectsWithTag("patrol points");
+        agent.updatePosition = false;
+        GoToNextPoint();
+    }
+
+    // Update is called once per frame
+    void GoToNextPoint()
+    {
+        if(points.Length == 0)
+        {
+            return;
+        }
+        agent.destination = points[destPoint].transform.position;
+        destPoint = (destPoint + 1) % points.Length;
+
+    }
+
+    void Update()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GoToNextPoint();
+        Vector3 worldDeltaPosition = agent.nextPosition - transform.position;
+
+        // Map 'worldDeltaPosition' to local space
+        float dx = Vector3.Dot(transform.right, worldDeltaPosition);
+        float dy = Vector3.Dot(transform.forward, worldDeltaPosition);
+        Vector2 deltaPosition = new Vector2(dx, dy);
+
+        // Low-pass filter the deltaMove
+        float smooth = Mathf.Min(1.0f, Time.deltaTime / 0.15f);
+        smoothDeltaPosition = Vector2.Lerp(smoothDeltaPosition, deltaPosition, smooth);
+
+        // Update velocity if time advances
+        if (Time.deltaTime > 1e-5f)
+            velocity = smoothDeltaPosition / Time.deltaTime;
+
+        bool shouldMove = velocity.magnitude > 0.5f && agent.remainingDistance > agent.radius;
+
+        // Update animation parameters
+        anim.SetBool("move", shouldMove);
+        anim.SetFloat("velx", velocity.x);
+        anim.SetFloat("vely", velocity.y);
+        // Pull agent towards character
+        if (worldDeltaPosition.magnitude > agent.radius)
+            agent.nextPosition = transform.position + 0.9f * worldDeltaPosition;
+    }
+
+    void OnAnimatorMove()
+    {
+        // Update position based on animation movement using navigation surface height
+        Vector3 position = anim.rootPosition;
+        position.y = agent.nextPosition.y;
+        transform.position = position;
+    }
+}
